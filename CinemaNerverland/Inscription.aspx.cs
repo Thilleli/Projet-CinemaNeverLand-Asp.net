cb13994ca6374b974d99dee1f8b3185ad6191b09
Alexandra Wasef <Alexandra.wasef@etu.cyu.fr>
2021-03-16 19:28:04 +0100

    Mise en place de condition dans la page inscription

--- CinemaNerverland/Inscription.aspx.cs
+++ CinemaNerverland/Inscription.aspx.cs
@@ -24,39 +24,53 @@ namespace CinemaNerverland
             //Response.Write("Connection Réussie");
             cnn.Close();
 
+
+
         }
         protected void send_Click(object sender, EventArgs e)
         {
+            
             if (InscriptionNom.Text!="" && InscriptionName.Text!="" && InscriptionAge.Text != "" && InscriptionMail.Text != "" && InscriptionID.Text != "" && InscriptionMDP.Text != "" && InscriptionMDPC.Text != "") { 
+                
                 if(InscriptionMDP.Text != InscriptionMDPC.Text)
                 {
-                    Response.Write("Les deux mot de passe ne coresspondent pas ");
+                    ErrorMessage.Text = "Les deux mot de passe ne coresspondent pas";
+
+                }
+                else 
+                {
+                    string connetionString;
+                    MySqlConnection cnn;
+
+                    connetionString = @"Data Source=mysql-cinemaneverland.alwaysdata.net;Database=cinemaneverland_data ;User ID=219115_wb;Password=wasefbelhocine01*";
+
+                    cnn = new MySqlConnection(connetionString);
+                    cnn.Open();
+                    MySqlCommand command;
+                    MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    String sql = "INSERT INTO `user`(id_user,nom_user,prenom_user,age_user,mail_user,login_user,mdp_user) VALUES('', @_Nom , @_Name , @_Age ,@_Mail, @_ID , @_Password )";
+
+                    command = new MySqlCommand(sql, cnn);
+
+                    adapter.InsertCommand = command;
+
+                    command.Parameters.AddWithValue("@_Nom", InscriptionNom.Text);
+                    command.Parameters.AddWithValue("@_Name", InscriptionName.Text);
+                    command.Parameters.AddWithValue("@_Age", InscriptionAge.Text);
+                    command.Parameters.AddWithValue("@_Mail", InscriptionMail.Text);
+                    command.Parameters.AddWithValue("@_ID", InscriptionID.Text);
+                    command.Parameters.AddWithValue("@_Password", InscriptionMDP.Text);
+
+                    adapter.InsertCommand.ExecuteNonQuery();
+                    command.Dispose();
+
+                    cnn.Close();
                 }
             }
             else
             {
-                Response.Write("Veuillez remplir tout les champs ");
+                ErrorMessage.Text = "Veuillez remplir tout les champs " ;
             }
-            Session["Nom"] = InscriptionID.Text;
-            Response.Write(Session["Nom"]);
-            /*string connetionString;
-            MySqlConnection cnn;
-
-            connetionString = @"Server=DESKTOP-ISBOFB1\SQLEXPRESS;Trusted_Connection=True;Database=demodb ;User ID=Mylogin;Password=123";
-
-            cnn = new MySqlConnection(connetionString);
-            cnn.Open();
-            MySqlCommand command;
-            MySqlDataAdapter adapter = new MySqlDataAdapter();
-            String sql = "SELECT login_user, mdp_user, nom_user, prenom_user, age_user, mail_user, id_user FROM user ";
-
-            command = new MySqlCommand(sql, cnn);
-            adapter.InsertCommand = command;
-            adapter.InsertCommand.ExecuteNonQuery();
-
-            command.Dispose();
-
-            cnn.Close();*/
         }
     }
 }
