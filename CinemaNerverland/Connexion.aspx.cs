554a3eeefc0b2750912fbadee21b4506ff363a91
Alexandra Wasef <Alexandra.wasef@etu.cyu.fr>
2021-03-22 15:11:32 +0100

    fgerg

--- CinemaNerverland/Connexion.aspx.cs
+++ CinemaNerverland/Connexion.aspx.cs
@@ -11,7 +11,7 @@ namespace CinemaNerverland
 {
     public partial class Connexion : System.Web.UI.Page
     {
-        
+
         public string Texte { get; private set; }
 
         protected void Page_Load(object sender, EventArgs e)
@@ -30,32 +30,32 @@ namespace CinemaNerverland
         }
         protected void send_Click(object sender, EventArgs e)
         {
-           
-                string connetionString;
-                connetionString = @"Data Source=mysql-cinemaneverland.alwaysdata.net;Database=cinemaneverland_data ;User ID=219115_wb;Password=wasefbelhocine01*";
-                //connetionString = @"Server=tcp:myservertuto.database.windows.net,1433;Initial Catalog=mydbtuto;Persist Security Info=False;User ID=myadmin;Password=Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-                MySqlConnection cnn = new MySqlConnection(connetionString);
+            string connetionString;
+            connetionString = @"Data Source=mysql-cinemaneverland.alwaysdata.net;Database=cinemaneverland_data ;User ID=219115_wb;Password=wasefbelhocine01*";
+            //connetionString = @"Server=tcp:myservertuto.database.windows.net,1433;Initial Catalog=mydbtuto;Persist Security Info=False;User ID=myadmin;Password=Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-                cnn.Open();
-                MySqlCommand cmd = new MySqlCommand("select count(*) from user where login_user='"+connexionID.Text+ "' and mdp_user='"+ConnexionMDP.Text+"' ", cnn);
-                MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
-                System.Data.DataTable dt = new System.Data.DataTable();
-                sda.Fill(dt);
-                cmd.ExecuteNonQuery();
-                if (dt.Rows[0][0].ToString() == "1")
-                {
-                    ErrorMessage.Text = "Vous êtes bien connecté";
-                }
-                else
-                {
-                    ErrorMessage.Text = "Utilisateur introuvable!";
-                }
+            MySqlConnection cnn = new MySqlConnection(connetionString);
+
+            cnn.Open();
+            MySqlCommand cmd = new MySqlCommand("select count(*) from user where login_user='" + connexionID.Text + "' and mdp_user='" + ConnexionMDP.Text + "' ", cnn);
+            MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+            System.Data.DataTable dt = new System.Data.DataTable();
+            sda.Fill(dt);
+            cmd.ExecuteNonQuery();
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                ErrorMessage.Text = "Vous êtes bien connecté";
+            }
+            else
+            {
+                ErrorMessage.Text = "Utilisateur introuvable!";
+            }
 
 
-                cnn.Close();
+            cnn.Close();
 
-            
+
         }
 
     }
