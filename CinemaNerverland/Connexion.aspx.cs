cb1a7ecec4162e4d5b9e658e2197d369867e470f
Alexandra Wasef <Alexandra.wasef@etu.cyu.fr>
2021-03-23 15:06:15 +0100

    Hashage du mot de passe en sha1

--- CinemaNerverland/Connexion.aspx.cs
+++ CinemaNerverland/Connexion.aspx.cs
@@ -5,15 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using MySql.Data.MySqlClient;
-using System.Web.Configuration;
 
 namespace CinemaNerverland
 {
     public partial class Connexion : System.Web.UI.Page
     {
-
-        public string Texte { get; private set; }
-
         protected void Page_Load(object sender, EventArgs e)
         {
             string connetionString;
@@ -30,14 +26,14 @@ namespace CinemaNerverland
         }
         protected void send_Click(object sender, EventArgs e)
         {
-
             string connetionString;
+            MySqlConnection cnn;
+
             connetionString = @"Data Source=mysql-cinemaneverland.alwaysdata.net;Database=cinemaneverland_data ;User ID=219115_wb;Password=wasefbelhocine01*";
-            //connetionString = @"Server=tcp:myservertuto.database.windows.net,1433;Initial Catalog=mydbtuto;Persist Security Info=False;User ID=myadmin;Password=Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-            MySqlConnection cnn = new MySqlConnection(connetionString);
-
+            cnn = new MySqlConnection(connetionString);
             cnn.Open();
+            ConnexionMDP.Text = Hasher.HashString(ConnexionMDP.Text.Trim());//hachage du mdp en sha1
             MySqlCommand cmd = new MySqlCommand("select count(*) from user where login_user='" + connexionID.Text + "' and mdp_user='" + ConnexionMDP.Text + "' ", cnn);
             MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
             System.Data.DataTable dt = new System.Data.DataTable();
@@ -52,10 +48,7 @@ namespace CinemaNerverland
                 ErrorMessage.Text = "Utilisateur introuvable!";
             }
 
-
             cnn.Close();
-
-
         }
 
     }
